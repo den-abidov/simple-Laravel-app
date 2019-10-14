7f2ac8a90c51cf71fc616113c15f5506351e5bcf
Denis Abidov <den-abidov@yandex.ru>
2019-10-14 21:24:41 +0300

    Создал news.blad.php, организовал стили welcome.scss и news.scss.

---
+++ resources/views/news.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+ 
+  <title>Новости</title>
+ 
+  <!-- CSS -->
+  <link rel="stylesheet" href="{{asset('css/app.css')}}">
+  <link rel="stylesheet" href="{{asset('css/news.css')}}">
+
+</head>
+<body>
+
+  <h1 class="center">Новости</h1>
+
+  <section id="news-block">
+    <p>Список новостей:</p>
+    <ul>
+      <li></li>
+    </ul>
+  </section>
+  <p class="center"><a href="/">< Назад</a></p>
+
+  
+</body>
+</html>
